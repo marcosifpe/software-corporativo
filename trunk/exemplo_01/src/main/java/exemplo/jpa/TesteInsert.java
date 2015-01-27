9f3b08d67fbfbe3db05f7cd2dbd0e56d58b10a1c
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-01-27 02:08:23 +0000

    Atualização de TestInsert e persistence.xml para criação automática de tabelas.

--- trunk/exemplo_01/src/main/java/exemplo/jpa/TesteInsert.java
+++ trunk/exemplo_01/src/main/java/exemplo/jpa/TesteInsert.java
@@ -2,24 +2,41 @@ package exemplo.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class TesteInsert {
 
     public static void main(String[] args) {
-        //EntityManagerFactory realiza a leitura das informações relativas à "persistence-unit".
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemplo_01");
-        //Criação do EntityManager
-        EntityManager em = factory.createEntityManager();
-        em.getTransaction().begin(); //Início da transação
         Usuario usuario = new Usuario();
+        preencherUsuario(usuario);
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        EntityTransaction et = null;
+        System.out.println("aqui");
+        try {
+            //EntityManagerFactory realiza a leitura das informações relativas à "persistence-unit".
+            emf = Persistence.createEntityManagerFactory("exemplo_01");
+            em = emf.createEntityManager(); //Criação do EntityManager.
+            et = em.getTransaction();
+            et.begin();
+            em.persist(usuario);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null)
+                et.rollback();
+        } finally {
+            if (em != null)
+                em.close();       
+            if (emf != null)
+                emf.close();
+        }
+    }
+
+    private static void preencherUsuario(Usuario usuario) {
         usuario.setNome("Marcos Costa");
         usuario.setEmail("marcoscosta@recife.ifpe.edu.br");
         usuario.setLogin("marcoscosta");
         usuario.setSenha("teste");
-        em.persist(usuario);
-        em.getTransaction().commit(); //Commit da transação
-        em.close();
-        factory.close();
     }
 }
