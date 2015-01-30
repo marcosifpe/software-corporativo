4d20a108d217d8eba9c4b5e805e83cf96a75c799
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-01-30 01:39:46 +0000

    Exemplo 02. Versão inicial.

---
+++ trunk/exemplo_02/src/main/java/exemplo/jpa/TesteJPA.java
@@ -0,0 +1,51 @@
+package exemplo.jpa;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        preencherUsuario(usuario);
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        EntityTransaction et = null;
+        try {
+            //EntityManagerFactory realiza a leitura das informações relativas à "persistence-unit".
+            emf = Persistence.createEntityManagerFactory("exemplo_02");
+            em = emf.createEntityManager(); //Criação do EntityManager.
+            et = em.getTransaction(); //Recupera objeto responsável pelo gerenciamento de transação.
+            et.begin();
+            em.persist(usuario);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null && et.isActive())
+                et.rollback();
+        } finally {
+            if (em != null)
+                em.close();       
+            if (emf != null)
+                emf.close();
+        }
+    }
+
+    private static void preencherUsuario(Usuario usuario) {
+        usuario.setNome("Fulano da Silva");
+        usuario.setEmail("fulano@gmail.com");
+        usuario.setLogin("fulano");
+        usuario.setSenha("teste");
+        usuario.setCpf("534.585.764-49");     
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, 1981);
+        c.set(Calendar.MONTH, Calendar.FEBRUARY);
+        c.set(Calendar.DAY_OF_MONTH, 25);
+        usuario.setDataNascimento(c.getTime());
+    }
+}
