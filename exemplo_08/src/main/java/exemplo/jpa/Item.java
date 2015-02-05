e6c0a46281c0a930fa06c8161ff40537be29296e
marcosifpe <marcosifpe@gmail.com>
2015-02-05 19:06:17 +0000

    versão inicial.

---
+++ exemplo_08/src/main/java/exemplo/jpa/Item.java
@@ -0,0 +1,89 @@
+package exemplo.jpa;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_ITEM")
+public class Item implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany(mappedBy = "item", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Oferta> ofertas;
+    @Column(name = "TXT_TITULO", length = 150, nullable = false)
+    private String titulo;
+    @Column(name = "TXT_DESCRICAO", length = 500, nullable = false)
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Oferta> getOfertas() {
+        return ofertas;
+    }
+
+    public void addOferta(Oferta oferta) {
+        if (this.ofertas == null) {
+            this.ofertas = new ArrayList<>();
+        }
+        
+        this.ofertas.add(oferta);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Item)) {
+            return false;
+        }
+        Item other = (Item) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "exemplo.jpa.Item[ id=" + id + " ]";
+    }
+    
+}
