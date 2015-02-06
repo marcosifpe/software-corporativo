87f05d742776c04b6775dfe3a5d0fc8b74f2af10
marcosifpe <marcosifpe@gmail.com>
2015-02-06 02:51:12 +0000

    Atualização.

--- exemplo_08/src/main/java/exemplo/jpa/TesteJPA.java
+++ exemplo_08/src/main/java/exemplo/jpa/TesteJPA.java
@@ -1,11 +1,13 @@
 package exemplo.jpa;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class TesteJPA {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo_08");
@@ -53,10 +55,41 @@ public class TesteJPA {
         } finally {
             em.close();
         }
-
     }
 
     private static void criarItens() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        Item item = new Item();
+        Oferta oferta1 = new Oferta();
+        Oferta oferta2 = new Oferta();
         
+        try {
+            et.begin();                        
+            item.setDescricao("POD Studio");
+            item.setDescricao("Interface para gravação de instrumentos musicais");
+            Query query = em.createQuery("SELECT c FROM Categoria c");
+            List<Categoria> categorias = query.getResultList();
+            for (Categoria categoria : categorias) {
+                item.adicionar(categoria);
+            }
+            
+            oferta1.setValor(250.00);
+            oferta2.setValor(275.00);
+            item.adicionar(oferta1);
+            item.adicionar(oferta2);
+            
+            em.persist(item);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null && et.isActive()) {
+                Logger.getGlobal().log(Level.SEVERE,
+                        "Cancelando transação com erro. Mensagem: {0}", ex.getMessage());
+                et.rollback();
+                Logger.getGlobal().info("Transação cancelada.");
+            }
+        } finally {
+            em.close();
+        }        
     }
 }
