a6ba4d5e4ad776f13555592e68cbbd5d676b6246
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-02-06 02:31:21 +0000

    Atualização.

--- trunk/exemplo_08/src/main/java/exemplo/jpa/Categoria.java
+++ trunk/exemplo_08/src/main/java/exemplo/jpa/Categoria.java
@@ -1,13 +1,12 @@
 package exemplo.jpa;
 
 import java.io.Serializable;
-import java.util.List;
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
--- trunk/exemplo_08/src/main/java/exemplo/jpa/TesteJPA.java
+++ trunk/exemplo_08/src/main/java/exemplo/jpa/TesteJPA.java
@@ -2,13 +2,56 @@ package exemplo.jpa;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class TesteJPA {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo_08");
+    private static final Logger logger = Logger.getGlobal();
 
     static {
-        Logger.getGlobal().setLevel(Level.INFO);
+        logger.setLevel(Level.INFO);
+    }
+    
+    public static void main(String[] args) {
+        try {
+            criarCategorias();
+        } finally {
+            emf.close();
+        }
+    }
+    
+    public static void criarCategorias() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        Categoria categoria1 = new Categoria();
+        categoria1.setNome("Informática");
+        Categoria categoria2 = new Categoria();
+        categoria2.setNome("Hardware");
+        Categoria categoria3 = new Categoria();
+        categoria3.setNome("Software");
+        Categoria categoria4 = new Categoria();
+        categoria4.setNome("Equipamentos Musicais");
+        
+        try {
+            et.begin();
+            em.persist(categoria1);
+            em.persist(categoria2);
+            em.persist(categoria3);
+            em.persist(categoria4);
+            et.commit();
+        } catch (Exception ex) {
+            if (et != null && et.isActive()) {
+                Logger.getGlobal().log(Level.SEVERE,
+                        "Cancelando transação com erro. Mensagem: {0}", ex.getMessage());
+                et.rollback();
+                Logger.getGlobal().info("Transação cancelada.");
+            }
+        } finally {
+            em.close();
+        }
+
     }
 }
