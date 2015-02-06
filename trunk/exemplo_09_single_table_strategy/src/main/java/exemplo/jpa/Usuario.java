24583388e4835352349465542bd9ae86b5d6b8c2
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-02-06 17:34:55 +0000

    Atualização one to many e many to one (bidirecional).

--- trunk/exemplo_09_single_table_strategy/src/main/java/exemplo/jpa/Usuario.java
+++ trunk/exemplo_09_single_table_strategy/src/main/java/exemplo/jpa/Usuario.java
@@ -37,18 +37,18 @@ public abstract class Usuario implements Serializable {
     protected Endereco endereco;
     @ElementCollection
     @CollectionTable(name = "TB_TELEFONE",
-            joinColumns = @JoinColumn(name = "ID_USUARIO", nullable = false))
-    @Column(name = "TXT_NUM_TELEFONE", nullable = false, length = 20)
+            joinColumns = @JoinColumn(name = "ID_USUARIO"))
+    @Column(name = "TXT_NUM_TELEFONE")
     protected Collection<String> telefones;
-    @Column(name = "TXT_CPF", nullable = false, length = 14, unique = true)
+    @Column(name = "TXT_CPF")
     protected String cpf;
-    @Column(name = "TXT_LOGIN", nullable = false, length = 50, unique = true)
+    @Column(name = "TXT_LOGIN")
     protected String login;
-    @Column(name = "TXT_NOME", nullable = false, length = 255)
+    @Column(name = "TXT_NOME")
     protected String nome;
-    @Column(name = "TXT_EMAIL", nullable = false, length = 50)
+    @Column(name = "TXT_EMAIL")
     protected String email;
-    @Column(name = "TXT_SENHA", nullable = false, length = 20)
+    @Column(name = "TXT_SENHA")
     protected String senha;
     @Temporal(TemporalType.DATE)
     @Column(name = "DT_NASCIMENTO", nullable = true)
