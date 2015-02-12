62599beee58d2fba27a95237ca78eb76e2008487
marcosifpe <marcosifpe@gmail.com>
2015-02-12 03:06:43 +0000

    Atualização.

--- exemplo_11_JPQL/src/test/java/exemplo/jpa/test/JpqlTest.java
+++ exemplo_11_JPQL/src/test/java/exemplo/jpa/test/JpqlTest.java
@@ -3,7 +3,9 @@ package exemplo.jpa.test;
 import exemplo.jpa.Categoria;
 import exemplo.jpa.Comprador;
 import exemplo.jpa.DatasLimite;
+import exemplo.jpa.Usuario;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,30 +27,31 @@ import static org.junit.Assert.*;
  * @author MASC
  */
 public class JpqlTest {
+
     private static EntityManagerFactory emf;
     private static final Logger LOGGER = Logger.getGlobal();
     private EntityManager em;
-    
+
     public JpqlTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
         LOGGER.setLevel(Level.INFO);
         emf = Persistence.createEntityManagerFactory("exemplo_11");
         DbUnitUtil.inserirDados();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
         emf.close();
     }
-    
+
     @Before
     public void setUp() {
         em = emf.createEntityManager();
     }
-    
+
     @After
     public void tearDown() {
         em.close();
@@ -57,34 +60,34 @@ public class JpqlTest {
     @Test
     public void categoriaPorNome() {
         TypedQuery<Categoria> query = em.createQuery(
-                "SELECT c FROM Categoria c WHERE c.nome LIKE :nome ORDER BY c.id", 
+                "SELECT c FROM Categoria c WHERE c.nome LIKE :nome ORDER BY c.id",
                 Categoria.class);
         query.setParameter("nome", "Instrumentos%");
         List<Categoria> categorias = query.getResultList();
-        
+
         for (Categoria categoria : categorias) {
             assertTrue(categoria.getNome().startsWith("Instrumentos"));
-        }        
+        }
     }
-    
+
     @Test
     public void categoriaPorNome2() {
         TypedQuery<Categoria> query = em.createNamedQuery("Categoria.PorNome", Categoria.class);
         query.setParameter("nome", "Instrumentos%");
         List<Categoria> categorias = query.getResultList();
-        
+
         for (Categoria categoria : categorias) {
             assertTrue(categoria.getNome().startsWith("Instrumentos"));
-        }        
+        }
     }
-    
+
     @Test
     public void categoriasQuantidadeFilhas() {
         TypedQuery<Long> query = em.createQuery(
                 "SELECT COUNT(c) FROM Categoria c WHERE c.mae IS NOT NULL", Long.class);
-        Long resultado = query.getSingleResult();        
+        Long resultado = query.getSingleResult();
         assertEquals(new Long(3), resultado);
-    }    
+    }
 
     @Test
     public void maximaMinimaDataNascimento() {
@@ -92,66 +95,66 @@ public class JpqlTest {
                 "SELECT MAX(c.dataNascimento), MIN(c.dataNascimento) FROM Comprador c");
         Object[] resultado = (Object[]) query.getSingleResult();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        String maiorData = dateFormat.format((Date)resultado[0]);
-        String menorData = dateFormat.format((Date)resultado[1]);
+        String maiorData = dateFormat.format((Date) resultado[0]);
+        String menorData = dateFormat.format((Date) resultado[1]);
         assertEquals("21-12-1999", maiorData);
         assertEquals("11-08-1973", menorData);
-    }    
+    }
 
     @Test
     public void datasLimite() {
         TypedQuery<DatasLimite> query = em.createQuery(
-                "SELECT NEW exemplo.jpa.DatasLimite(MAX(c.dataNascimento), MIN(c.dataNascimento)) FROM Comprador c", 
+                "SELECT NEW exemplo.jpa.DatasLimite(MAX(c.dataNascimento), MIN(c.dataNascimento)) FROM Comprador c",
                 DatasLimite.class);
         DatasLimite datasLimite = query.getSingleResult();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        String dataMaxima = dateFormat.format((Date)datasLimite.getDataMaxima());
-        String dataMinima = dateFormat.format((Date)datasLimite.getDataMinima());
+        String dataMaxima = dateFormat.format((Date) datasLimite.getDataMaxima());
+        String dataMinima = dateFormat.format((Date) datasLimite.getDataMinima());
         assertEquals("21-12-1999", dataMaxima);
         assertEquals("11-08-1973", dataMinima);
-    }    
-    
+    }
+
     @Test
     public void categoriasMaes() {
         TypedQuery<Categoria> query;
-        query = em.createQuery( 
+        query = em.createQuery(
                 "SELECT c FROM Categoria c WHERE c.filhas IS NOT EMPTY",
                 Categoria.class);
         List<Categoria> categorias = query.getResultList();
-        
+
         for (Categoria categoria : categorias) {
             assertTrue(!categoria.getFilhas().isEmpty());
-        }                
+        }
     }
 
     @Test
     public void usuariosVisa() {
         TypedQuery<Comprador> query;
-        query = em.createQuery( 
+        query = em.createQuery(
                 "SELECT c FROM Comprador c WHERE c.cartaoCredito.bandeira like ?1 ORDER BY c.nome DESC",
                 Comprador.class);
         query.setParameter(1, "VISA"); //Setando parâmetro posicional.
         query.setMaxResults(20); //Determinando quantidade máxima de resultados.
         List<Comprador> compradores = query.getResultList();
-        
+
         for (Comprador comprador : compradores) {
             assertEquals("VISA", comprador.getCartaoCredito().getBandeira());
-            
-        }                
+
+        }
     }
-    
+
     @Test
     public void compradoresVisaOuMaster() {
         TypedQuery<Comprador> query;
-        query = em.createQuery( 
-                "SELECT c FROM Comprador c " +
-                "WHERE c.cartaoCredito.bandeira LIKE ?1 " +
-                "OR c.cartaoCredito.bandeira LIKE ?2 ORDER BY c.nome DESC",
+        query = em.createQuery(
+                "SELECT c FROM Comprador c "
+                + "WHERE c.cartaoCredito.bandeira LIKE ?1 "
+                + "OR c.cartaoCredito.bandeira LIKE ?2 ORDER BY c.nome DESC",
                 Comprador.class);
         query.setParameter(1, "VISA"); //Setando parâmetro posicional.
         query.setParameter(2, "MASTERCARD"); //Setando parâmetro posicional.        
         List<Comprador> compradores = query.getResultList();
-        
+
         for (Comprador comprador : compradores) {
             switch (comprador.getCartaoCredito().getBandeira()) {
                 case "VISA":
@@ -164,18 +167,18 @@ public class JpqlTest {
                     assertTrue(false);
                     break;
             }
-        }                
+        }
     }
-    
+
     @Test
     public void compradoresInMastercardMaestro() {
         TypedQuery<Comprador> query;
-        query = em.createQuery( 
-                "SELECT c FROM Comprador c " +
-                "WHERE c.cartaoCredito.bandeira IN ('MAESTRO', 'MASTERCARD') ORDER BY c.nome DESC",
+        query = em.createQuery(
+                "SELECT c FROM Comprador c "
+                + "WHERE c.cartaoCredito.bandeira IN ('MAESTRO', 'MASTERCARD') ORDER BY c.nome DESC",
                 Comprador.class);
         List<Comprador> compradores = query.getResultList();
-        
+
         for (Comprador comprador : compradores) {
             switch (comprador.getCartaoCredito().getBandeira()) {
                 case "MAESTRO":
@@ -188,7 +191,29 @@ public class JpqlTest {
                     assertTrue(false);
                     break;
             }
-        }        
+        }
     }
-    
+
+    @Test
+    public void usuariosPorDataNascimento() {
+        TypedQuery<Usuario> query;
+        query = em.createQuery(
+                "SELECT u FROM Usuario u WHERE u.dataNascimento BETWEEN ?1 AND ?2",
+                Usuario.class);
+        query.setParameter(1, getData(21, Calendar.FEBRUARY, 1980));
+        query.setParameter(2, getData(1, Calendar.DECEMBER, 1990));
+        List<Usuario> usuarios = query.getResultList();
+        
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+    }
+
+    private Date getData(Integer dia, Integer mes, Integer ano) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, ano);
+        c.set(Calendar.MONTH, mes);
+        c.set(Calendar.DAY_OF_MONTH, dia);
+        return c.getTime();
+    }
 }
