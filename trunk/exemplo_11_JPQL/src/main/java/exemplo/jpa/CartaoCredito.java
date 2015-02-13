9ac3f83d52f15ba7136e9f4e515dee54c6396486
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-02-13 04:23:35 +0000

    Atualização.

--- trunk/exemplo_11_JPQL/src/main/java/exemplo/jpa/CartaoCredito.java
+++ trunk/exemplo_11_JPQL/src/main/java/exemplo/jpa/CartaoCredito.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.SqlResultSetMappings;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
