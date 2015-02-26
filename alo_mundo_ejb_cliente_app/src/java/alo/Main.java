1089b9631d24afc33d99ed2d9e0ce6b3ff90b7e1
marcosifpe <marcosifpe@gmail.com>
2015-02-26 04:38:55 +0000

    Atualização.

--- alo_mundo_ejb_cliente_app/src/java/alo/Main.java
+++ alo_mundo_ejb_cliente_app/src/java/alo/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(aloBean.getMensagem("Marcos"));
+        System.out.println(aloBean.getMensagem("Teste"));
     }
     
 }
