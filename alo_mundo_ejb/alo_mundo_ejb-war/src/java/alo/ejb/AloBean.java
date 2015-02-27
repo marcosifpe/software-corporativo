77d01e3adf103556f081bb41e95ca840d532e835
marcosifpe <marcosifpe@gmail.com>
2015-02-27 02:46:12 +0000

    Atualização.

--- alo_mundo_ejb/alo_mundo_ejb-war/src/java/alo/ejb/AloBean.java
+++ alo_mundo_ejb/alo_mundo_ejb-war/src/java/alo/ejb/AloBean.java
@@ -3,8 +3,9 @@ package alo.ejb;
 import javax.ejb.Stateless;
 
 @Stateless(name = "Alo")
-public class AloBean {
+public class AloBean implements Alo {
 
+    @Override
     public String getMensagem(String nome) {
         return String.format("Alô %s, bem vindo ao EJB 3!", nome);
     }
