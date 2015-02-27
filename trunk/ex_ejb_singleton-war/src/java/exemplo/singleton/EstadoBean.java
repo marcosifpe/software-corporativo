17dcc1f2b05d7af1d41733ffa409e61c392d80fe
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-02-27 20:35:09 +0000

    versão inicial

---
+++ trunk/ex_ejb_singleton-war/src/java/exemplo/singleton/EstadoBean.java
@@ -0,0 +1,72 @@
+package exemplo.singleton;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author MASC
+ */
+@Singleton
+public class EstadoBean implements EstadoBeanLocal {
+    @Resource(name = "jdbc/__ejb_singleton")
+    private DataSource dataSource;
+    private List<Estado> estados = new ArrayList<>();
+
+    @PostConstruct
+    public void inicializar() {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT txt_sigla as sigla, txt_nome as nome FROM tb_estado ORDER BY 1";
+        
+        try {
+            con = dataSource.getConnection();            
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                Estado estado = new Estado();
+                estado.setSigla(rs.getString(1));
+                estado.setNome(rs.getString(2));
+                estados.add(estado);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        } finally {
+            fecharRecursos(rs, stmt, con);
+        }
+    }
+
+    private void fecharRecursos(ResultSet rs, Statement stmt, Connection con) throws RuntimeException {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            
+            if (stmt != null) {
+                stmt.close();
+            }
+            
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public List<Estado> consultarEstados() {
+        return this.estados;
+    }
+
+}
