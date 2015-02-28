278796cb7e2f9ee8cf395ebe689a78a3aed7c034
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-02-28 02:28:58 +0000

    Atualização.

---
+++ trunk/ex_ejb_singleton-war/src/java/exemplo/singleton/ConfiguradorBean.java
@@ -0,0 +1,17 @@
+package exemplo.singleton;
+
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author MASC
+ */
+@Singleton
+@LocalBean
+public class ConfiguradorBean {
+
+    public void algumMetodo() {
+        
+    }
+}
