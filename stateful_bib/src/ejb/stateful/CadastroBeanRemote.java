594e6bc0185d4ceab2d0347dbdd0df9b3609e56a
marcosifpe <marcosifpe@gmail.com>
2015-03-04 18:52:43 +0000

    versão inicial

---
+++ stateful_bib/src/ejb/stateful/CadastroBeanRemote.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.stateful;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author MASC
+ */
+@Remote
+public interface CadastroBeanRemote {
+
+    public boolean conexaoAtiva();
+    public void criarUsuario(String login, String senha);
+    public void criarEnderecoUsuario(String cep, String logradouro, Integer numero);
+    public void cadastrarUsuario();
+    public void cancelar();
+}
