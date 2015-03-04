5fd56d8620de031154ba4c277a74b3fcde632a10
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-03-04 18:33:05 +0000

    versão inicial

---
+++ trunk/stateful_cliente/src/java/cliente/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+import ejb.stateful.CadastroBeanRemote;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author MASC
+ */
+public class Main {
+    @EJB(lookup = "java:global/stateful_bean/CadastroBean!ejb.stateful.CadastroBeanRemote")
+    private static CadastroBeanRemote cadastroBean;
+    
+    static {
+        Logger.getGlobal().setLevel(Level.INFO);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cadastroBean.criarUsuario("fulano", "teste123");
+        cadastroBean.criarEnderecoUsuario("50.250-120", "Avenida Beberibe", 100);
+        cadastroBean.cadastrarUsuario();
+    }
+    
+}
