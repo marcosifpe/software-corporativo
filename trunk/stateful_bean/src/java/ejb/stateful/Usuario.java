6b46a224064560a927944638063103a526e44cdd
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-03-04 18:55:17 +0000

    versão inicial

---
+++ trunk/stateful_bean/src/java/ejb/stateful/Usuario.java
@@ -0,0 +1,48 @@
+package ejb.stateful;
+
+/**
+ *
+ * @author MASC
+ */
+public class Usuario {
+    private String login;
+    private String senha;
+    private Endereco endereco;
+
+    public Usuario() {
+        
+    }
+    
+    public Usuario(String login, String senha) {
+        this.setLogin(login);
+        this.setSenha(senha);
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }    
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    public void criarEndereco(String cep, String logradouro, Integer numero) {
+        setEndereco(new Endereco(cep, logradouro, numero));
+    }
+}
