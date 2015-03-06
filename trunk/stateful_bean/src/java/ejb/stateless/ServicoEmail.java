089d2a344e9c445fa1646ba09aaa92f25ea29f6b
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-03-06 02:26:31 +0000

    Atualização.

--- trunk/stateful_bean/src/java/ejb/stateless/ServicoEmail.java
+++ trunk/stateful_bean/src/java/ejb/stateless/ServicoEmail.java
@@ -21,19 +21,17 @@ import javax.mail.internet.MimeMessage;
 public class ServicoEmail implements ServicoEmailLocal {
     @Resource(name = "mail/servicoEmailSession")
     private Session sessao;
-    @Resource(name = "mail/servicoEmailProperties")
-    private Properties propriedades;
 
     @Override
     @Asynchronous
     public void enviarMensagem(String para) {
         try {
             Message message = new MimeMessage(sessao);
-            message.setFrom(new InternetAddress(propriedades.getProperty("de")));
+            message.setFrom(new InternetAddress("discsoftwarecorporativo@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(para));
-            message.setSubject(propriedades.getProperty("assunto"));
-            message.setText(propriedades.getProperty("mensagem"));
+            message.setSubject("Cadastro realizado com sucesso");
+            message.setText("Cadastro realizado com sucesso. E-mail automático.");
             Transport.send(message);
         } catch (MessagingException ex) {
             Logger.getLogger(ServicoEmail.class.getName()).log(Level.SEVERE, null, ex);
