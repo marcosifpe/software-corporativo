05eae8ad89ce9274d0d3cf8d88c11dfe6fb67430
marcosifpe <marcosifpe@gmail.com>
2015-03-07 23:23:04 +0000

    versão inicial.

---
+++ exemplo_12_bean_validation/src/main/java/exemplo/jpa/Oferta.java
@@ -0,0 +1,115 @@
+package exemplo.jpa;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "TB_OFERTA")
+public class Oferta implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_ITEM", referencedColumnName = "ID")
+    private Item item;
+    @Column(name = "NUM_VALOR", nullable = false)
+    private Double valor;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_OFERTA", nullable = false)
+    private Date data;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_COMPRADOR", referencedColumnName = "ID_USUARIO", nullable = false)
+    private Comprador comprador;
+    @Column(name = "FLAG_VENCEDORA", nullable = false)
+    private boolean vencedora;    
+    
+    public Oferta() {
+        this.data = new Date();
+        this.vencedora = false;
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }    
+
+    public boolean isVencedora() {
+        return vencedora;
+    }
+
+    public void setVencedora(boolean vencedora) {
+        this.vencedora = vencedora;
+    }
+    
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Oferta)) {
+            return false;
+        }
+        Oferta other = (Oferta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "exemplo.jpa.Oferta[ id=" + id + " ]";
+    }
+    
+}
---
+++ exemplo_12_bean_validation/src/main/java/exemplo/jpa/Usuario.java
@@ -0,0 +1,182 @@
+package exemplo.jpa;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "TB_USUARIO")
+@Inheritance(strategy = InheritanceType.JOINED) //Estratégia de herança.
+@DiscriminatorColumn(name = "DISC_USUARIO", //Nome da coluna que vai discriminar subclasses.
+        discriminatorType = DiscriminatorType.STRING, length = 1)
+@Access(AccessType.FIELD)
+public abstract class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    @Embedded
+    protected Endereco endereco;
+    @ElementCollection
+    @CollectionTable(name = "TB_TELEFONE",
+            joinColumns = @JoinColumn(name = "ID_USUARIO"))
+    @Column(name = "TXT_NUM_TELEFONE")
+    protected Collection<String> telefones;
+    @Column(name = "TXT_CPF", length = 14, nullable = false, unique = true)
+    protected String cpf;
+    @Column(name = "TXT_LOGIN", length = 20, nullable = false, unique = true)
+    protected String login;
+    @Column(name = "TXT_NOME", length = 150, nullable = false)
+    protected String nome;
+    @Column(name = "TXT_EMAIL", length = 30, nullable = false)
+    protected String email;
+    @Column(name = "TXT_SENHA", length = 20, nullable = false)
+    protected String senha;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DT_NASCIMENTO", nullable = true)
+    protected Date dataNascimento;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_CRIACAO", nullable = true)
+    protected Date dataCriacao;
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Collection<String> getTelefones() {
+        return telefones;
+    }
+
+    public void addTelefone(String telefone) {
+        if (telefones == null) {
+            telefones = new HashSet<>();
+        }
+        telefones.add(telefone);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.id);
+        sb.append(", ");
+        sb.append(this.nome);
+        sb.append(", ");
+        sb.append(this.login);
+        sb.append(", ");
+        sb.append(this.cpf);
+
+        for (String telefone : this.telefones) {
+            sb.append(", ");
+            sb.append(telefone);
+        }
+
+        return sb.toString();
+    }
+}
