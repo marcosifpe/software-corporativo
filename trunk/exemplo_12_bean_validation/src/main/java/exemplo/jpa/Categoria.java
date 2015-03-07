4ae96f47cd0fd53eddb6abc4b2aee895e84add04
marcosifpe@gmail.com <marcosifpe@gmail.com@7ce2fa62-51c4-750d-3d4f-f6ce6bc2763e>
2015-03-07 23:23:04 +0000

    versão inicial.

---
+++ trunk/exemplo_12_bean_validation/src/main/java/exemplo/jpa/Categoria.java
@@ -0,0 +1,121 @@
+package exemplo.jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.Entity;
+import javax.persistence.EntityResult;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.Table;
+import org.hibernate.annotations.NamedNativeQueries;
+import org.hibernate.annotations.NamedNativeQuery;
+
+@Entity
+@Table(name = "TB_CATEGORIA")
+@NamedQueries(
+        {
+            @NamedQuery(
+                    name = "Categoria.PorNome",
+                    query = "SELECT c FROM Categoria c WHERE c.nome LIKE :nome ORDER BY c.id"
+            )
+        }
+)
+@NamedNativeQueries(
+        {
+            @NamedNativeQuery(
+                    name = "Categoria.PorNomeSQL",
+                    query = "SELECT id, txt_nome, id_categoria_mae FROM tb_categoria WHERE txt_nome LIKE ? ORDER BY id",
+                    resultClass = Categoria.class
+            ),
+            @NamedNativeQuery(
+                    name = "Categoria.QuantidadeItensSQL",
+                    query = "SELECT c.ID, c.TXT_NOME, c.ID_CATEGORIA_MAE, count(ic.ID_ITEM) as total_itens from tb_categoria c, tb_itens_categorias ic where c.TXT_NOME LIKE ? and c.ID = ic.ID_CATEGORIA GROUP BY c.id",
+                    resultSetMapping = "Categoria.QuantidadeItens"
+            )
+        }
+)
+@SqlResultSetMapping(
+        name = "Categoria.QuantidadeItens",
+        entities = {
+            @EntityResult(entityClass = Categoria.class)},
+        columns = {
+            @ColumnResult(name = "total_itens", type = Long.class)}
+)
+public class Categoria implements Serializable {
+
+    @Id
+    private Long id;
+    @Column(name = "TXT_NOME", length = 100, nullable = false, unique = true)
+    private String nome;
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "ID_CATEGORIA_MAE", referencedColumnName = "ID")
+    private Categoria mae;
+    @OneToMany(mappedBy = "mae", orphanRemoval = true)
+    private List<Categoria> filhas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Categoria getMae() {
+        return mae;
+    }
+
+    public void setMae(Categoria mae) {
+        this.mae = mae;
+    }
+
+    public List<Categoria> getFilhas() {
+        return filhas;
+    }
+
+    public boolean adicionar(Categoria categoria) {
+        return filhas.add(categoria);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Categoria)) {
+            return false;
+        }
+        Categoria other = (Categoria) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "exemplo.jpa.Tag[ id=" + id + ":" + nome + " ]";
+    }
+
+}
---
+++ trunk/exemplo_12_bean_validation/src/main/java/exemplo/jpa/Comprador.java
@@ -0,0 +1,49 @@
+package exemplo.jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_COMPRADOR") 
+@DiscriminatorValue(value = "C")
+@PrimaryKeyJoinColumn(name="ID_USUARIO", referencedColumnName = "ID")
+public class Comprador extends Usuario implements Serializable {
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = true)
+    @JoinColumn(name = "ID_CARTAO_CREDITO", referencedColumnName = "ID")
+    private CartaoCredito cartaoCredito;
+    @OneToMany(mappedBy = "comprador", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Oferta> ofertas;
+    
+    public CartaoCredito getCartaoCredito() {
+        return cartaoCredito;
+    }
+
+    public void setCartaoCredito(CartaoCredito cartaoCredito) {
+        this.cartaoCredito = cartaoCredito;
+        this.cartaoCredito.setDono(this);
+    }
+
+    public List<Oferta> getOfertas() {
+        return ofertas;
+    }
+
+    public boolean adicionar(Oferta oferta) {
+        return ofertas.add(oferta);
+    }
+
+    @Override
+    public String toString() {
+        return "exemplo.jpa.Comprador[ id=" + id + " ]";
+    }
+
+}
