90bf9d0959cc50db29d4e6f8373fa2582347039f
marcosifpe <marcosifpe@gmail.com>
2015-03-13 03:59:03 +0000

    Atualização.

--- exemplo_12_bean_validation/src/main/java/exemplo/jpa/Comprador.java
+++ exemplo_12_bean_validation/src/main/java/exemplo/jpa/Comprador.java
@@ -2,7 +2,6 @@ package exemplo.jpa;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
