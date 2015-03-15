edd8572df1577cc05394864b1c7a3add73a697b0
marcosifpe <marcosifpe@gmail.com>
2015-03-15 03:30:18 +0000

    Atualização.

--- exemplo_12_bean_validation/src/main/java/exemplo/jpa/CartaoCredito.java
+++ exemplo_12_bean_validation/src/main/java/exemplo/jpa/CartaoCredito.java
@@ -11,6 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
