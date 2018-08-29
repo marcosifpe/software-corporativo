da562b04fcad0b5c83ed6c7dd1edcb4838fb60cc
marcosifpe <marcosifpe@gmail.com>
2018-08-29 20:11:01 -0300

    Versao inicial

--- exemplo_11_JPQL/src/test/java/exemplo/jpa/test/JpqlTest.java
+++ exemplo_11_JPQL/src/test/java/exemplo/jpa/test/JpqlTest.java
@@ -12,7 +12,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import org.hamcrest.CoreMatchers;
